c9bd0d83be8ffd493598ad40a8f0f61226493571
Sahkal Poddar <sahkalplanet@gmail.com>
2018-07-13 15:22:38 +0530

    Add files via upload

--- js/signup.js
+++ js/signup.js
@@ -1,7 +1,7 @@
 $(".dropdown-trigger").dropdown();
 const menu = document.querySelector('.sidenav');
 M.Sidenav.init(menu, {});
-$('.dropdown-trigger').dropdown({
+$('.hover').dropdown({
       inDuration: 300,
       outDuration: 225,
       hover: true, // Activate on hover
